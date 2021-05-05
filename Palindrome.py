06be1624417e5ed1f7e1ffe0e4d1f5e72ff87937
Sandeep Kiran <kiransandeep9@gmail.com>
2021-05-05 16:58:36 +0530

    Created a two liner Palindrome code successfully

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,5 @@
-print("Hello world")
+#print("Palindrome Code")
+string = input("Your name: \n").upper()
+print("Palindrome" if string == string[::-1] else "Not palindrome")
+#print("Palindrome" if input("Your name: \n").upper() == [::-1] else "Not palindrome")
+
