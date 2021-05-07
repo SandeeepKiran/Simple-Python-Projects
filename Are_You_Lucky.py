dd7250ee84220661e9f848f98ef0b7863ad23376
Sandeep Kiran <kiransandeep9@gmail.com>
2021-05-07 18:51:52 +0530

    Finished a working Program that displays the correcct output

--- Are_You_Lucky.py
+++ Are_You_Lucky.py
@@ -4,9 +4,6 @@ Then tuning it into a 1 digit value, say if you got 157, it becomes 1+5+7 = 13 =
 Finally checking if the value obtained (4) is a fibonacci series number.
 If it is, output: You are Lucky, else output: You are Unlucky.
 '''
-def SingleDigit(val):
-    sum = 0 
-    while(sum > 9):
 
 name = input("Your Name: \n").lower()
 print(name)
@@ -15,10 +12,31 @@ for letter in name:
     val = val+ord(letter)-96
 print(val)
 
-'''
+if val % 9 == 0:
+    single_digit = val
+else:
+    single_digit = val % 9
+print(single_digit)
+
+x = 0
+y = 1
+if single_digit == x or single_digit == y:
+    print("Lucky")
+for i in range(6):
+    if single_digit == x + y:
+        print("Lucky")
+        x = 0
+        break
+    temp = x + y
+    x = y
+    y = temp
+if x != 0:
+    print("Unlucky")
+    
+''' My Code:
 if val>10:
     sum=0
-def single_digit(){
+def single_digit():
     while(val>10 and sum<10):
         sum = sum + int(val % 10)
         val = int(val/10)
@@ -26,11 +44,15 @@ def single_digit(){
         print(val)
         sum = = sum + int(val % 10)
     return sum 
-}
-while(sum<10):
-    single_digit()
+''' ''' GeeksforGeeks 0(1)
+def digSum(val):
+    sum = 0
+    while(val > 0 or sum > 9):
+        if(val == 0):
+            val = sum
+            sum = 0
+        sum += val % 10
+        val /= 10
+    return sum
 '''
-if val < 10:
-    sum = val
-else:
-    sum = SingleDigit(val)
+
